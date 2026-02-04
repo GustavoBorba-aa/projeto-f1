5ccba4e1b3d0878cc6896f674cb09762c47e1832
Gustavo Borba <gustavoborbavts@gmail.com>
2026-02-04 20:37:14 -0300

    Implementação de validações na service Team.

--- management/management/src/main/java/com/f1/management/service/TeamService.java
+++ management/management/src/main/java/com/f1/management/service/TeamService.java
@@ -22,6 +22,18 @@ public class TeamService {
     private TeamRepository teamRepository;
 
     public Team saveTeam(Team team) {
+        if (team.getName() == null || team.getName().isBlank()) {
+            throw new RuntimeException("O nome do EQUIPE não pode ser branco");
+        }
+        if (team.getCountry() == null || team.getCountry().isBlank()) {
+            throw new RuntimeException("O Páis de origem da equipe não pode estar vazio");
+        }
+        if (team.getCountry().length() < 2 || team.getCountry().length() > 20) {
+            throw new RuntimeException("O país deve ter entre 2 e 20 caracteres.");
+        }
+        if (team.getEngine() == null || team.getEngine().isBlank()) {
+            throw new RuntimeException("A fabricante do motor da equipe não pode está branco...");
+        }
         return teamRepository.save(team);
     }
 
@@ -32,13 +44,26 @@ public class TeamService {
 
     /**
      * Lógica para atualizar uma equipe.
-     * @param id ID da equipe que será modificada.
+     *
+     * @param id          ID da equipe que será modificada.
      * @param teamDetails Objeto com os novos dados vindos do Controller.
      */
     public Team updateTeam(Long id, Team teamDetails) {
         //Busca a equipe pelo ID,
         Team team = teamRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Equipe não encontrada com o id: " + id));
+        if (teamDetails.getName() == null || teamDetails.getName().isBlank()) {
+            throw new RuntimeException("O nome da EQUIPE não pode ser branco");
+        }
+        if (teamDetails.getCountry() == null || teamDetails.getCountry().isBlank()) {
+            throw new RuntimeException("O Páis de origem da equipe não pode estar vazio");
+        }
+        if (teamDetails.getCountry().length() < 2 || teamDetails.getCountry().length() > 20) {
+            throw new RuntimeException("O país deve ter entre 2 e 20 caracteres.");
+        }
+        if (teamDetails.getEngine() == null || teamDetails.getEngine().isBlank()) {
+            throw new RuntimeException("A fabricante do motor da equipe não pode está branco...");
+        }
         //Atualizamos os atributos do objeto que 'acabou de vir' do banco de dados
         team.setName(teamDetails.getName());
         team.setCountry(teamDetails.getCountry());
@@ -47,8 +72,7 @@ public class TeamService {
     }
 
 
-     // Lógica para deletar uma equipe.
-
+    // Lógica para deletar uma equipe.
     public void deleteTeam(Long id) {
         if (!teamRepository.existsById(id)) {
             throw new RuntimeException("Não é possível deletar: Equipe não encontrada.");
